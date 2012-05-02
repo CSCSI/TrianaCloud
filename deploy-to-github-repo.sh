0dd4d1f6fda8491c6af25ef7e68367428bfef7ad
Kieran David Evans <keyz182@gmail.com>
2012-05-02 16:29:24 +0100

    Fixed the deploy script

--- deploy-to-github-repo.sh
+++ deploy-to-github-repo.sh
@@ -1,5 +1,6 @@
 git clone git@github.com:keyz182/maven-repo.git /tmp/mvnrepo
 mvn -DaltDeploymentRepository=snapshot-repo::default::file:/tmp/mvnrepo/snapshots clean deploy
 cd /tmp/mvnrepo
-git commit -a --msg "Maven commit"
+git add *
+git commit -a -m "Maven commit"
 git push
